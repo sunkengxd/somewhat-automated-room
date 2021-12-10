0e4c74cd405fe715987505cc23c15dfcc9e1bd3f
sunkengxd <victor.gustiuc@mechatronics-center.com>
2021-12-10 21:23:41 +0200

    Added new pins.

--- include/pins.h
+++ include/pins.h
@@ -1,5 +1,8 @@
 #define MICROPHONE_PIN A0
 #define MQ_PIN         A1
+#define RGB_R          A2
+#define RGB_G          A3
+#define RGB_B          A4
 
 #define ECHO_PIN            2
 #define TRIGGER_PIN         3
@@ -7,3 +10,7 @@
 #define PAUSE_PLAY_BTN_PIN  5
 #define GET_TIME_BTN_PIN    6
 #define LIGHTS_PIN          7
+
+#define LED_COUNT 5
+
+const int LED_PINS[5] = { 8, 9, 10, 11, 12}
